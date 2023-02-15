bddffffee64ba5a0e3eb1050ea8c88b2c30ee5bb
Aditya Jain <200050003@iitb.ac.in>
2023-02-15 15:37:55 +0530

    rounding change

--- network.py
+++ network.py
@@ -2,15 +2,6 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import os
 import graphviz, argparse
-# G = nx.petersen_graph()
-
-# subax1 = plt.subplot(121)
-# nx.draw(G, with_labels = True, font_weight = 'bold')
-
-# subax2 = plt.subplot(122)
-# nx.draw_shell(G, nlist=[range(5, 10), range(5)], with_labels=True, font_weight='bold')
-
-# plt.show()
 
 import networkx as nx
 import random
@@ -93,7 +84,7 @@ for peer_index in range(num_trees):
         
         nodes.append(node)
         parents.append(parent)
-        times.append(round(time, 3))
+        times.append(round(time, 7))
         
         # print(f"node {node}, parent {parent}, time {time}")
         
